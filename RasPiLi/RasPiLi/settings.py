07988bd5641c48475ca0b81415917ec44fb3966d
Wikkily <87547943+Wikkily@users.noreply.github.com>
2021-08-15 03:55:02 +0200

    Staticfile working
    + html
    + css

--- RasPiLi/RasPiLi/settings.py
+++ RasPiLi/RasPiLi/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'RasPiLi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
 STATIC_URL = '/static/'
 
 # Default primary key field type
--- RasPiLi/pages/views.py
+++ RasPiLi/pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def navigation_view(request):
-    return render(request, 'navigation.html')
+    return render(request, 'navigation.html',{})
